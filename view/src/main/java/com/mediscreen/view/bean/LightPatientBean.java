4efe8e6a7bef0a5898c6b40ec0affba3a926e57e
Fercak Florian <fercak.florian@hotmail.fr>
2023-03-28 14:30:07 +0200

    ajout des signatures des methodes de la classe PatientRestController dans l'interface MicroservicePatientProxy, ajout de la classe LightPatientBean

---
+++ view/src/main/java/com/mediscreen/view/bean/LightPatientBean.java
@@ -0,0 +1,11 @@
+package com.mediscreen.view.bean;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LightPatientBean {
+    private String firstName;
+    private String lastName;
+}
