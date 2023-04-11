ad64749449fffed36364c9e770e13fd8577b8af1
Fercak Florian <fercak.florian@hotmail.fr>
2023-04-11 09:49:30 +0200

    test de l'analyse d'une note existante en base de donnee

--- report/src/main/java/com/mediscreen/report/service/ReportService.java
+++ report/src/main/java/com/mediscreen/report/service/ReportService.java
@@ -1,9 +1,14 @@
 package com.mediscreen.report.service;
 
+import com.mediscreen.report.bean.NoteBean;
 import com.mediscreen.report.model.Report;
+import com.mediscreen.report.proxy.MicroserviceNoteProxy;
 import com.mediscreen.report.utils.NoteParser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -11,8 +16,11 @@ public class ReportService {
 
     private NoteParser noteParser;
 
-    public ReportService(NoteParser noteParser) {
+    private MicroserviceNoteProxy microserviceNoteProxy;
+
+    public ReportService(NoteParser noteParser, MicroserviceNoteProxy microserviceNoteProxy) {
         this.noteParser = noteParser;
+        this.microserviceNoteProxy = microserviceNoteProxy;
     }
 
     public Report initReport() {
@@ -22,11 +30,31 @@ public class ReportService {
         /*-------------- testing if noteParser.count is working --------------*/
 
 
-        String note = "Taille Poids Fumeur TriggerOne TriggerTwo TriggerThree";
-        int result = noteParser.count(note);
-        if (result == 3) {
-            log.info("il y a bien " + result + " declencheurs");
+//        String note = "Taille Poids Fumeur TriggerOne TriggerTwo TriggerThree";
+//        int result = noteParser.count(note);
+//        if (result == 3) {
+//            log.info("il y a bien " + result + " declencheurs");
+//        }
+//        return report;
+
+        /*--------- récupération des notes ---------*/
+        List<NoteBean> notesBeans = new ArrayList<>();
+        try {
+            notesBeans = microserviceNoteProxy.getNotesByPatientId("6414bfae21e6ed573c1030f6");
+        } catch (Exception e) {
+            log.warn("error during retreiving notes");
         }
+
+        /*--------- analyse des notes ---------*/
+        int numberOfTriggers = 0;
+        for (NoteBean note : notesBeans) {
+            int count = noteParser.count(note.getContent());
+            if(count > 0){
+                numberOfTriggers = numberOfTriggers + count;
+            }
+
+        }
+        log.info("nombre de declencheurs : " + numberOfTriggers);
         return report;
     }
 }
