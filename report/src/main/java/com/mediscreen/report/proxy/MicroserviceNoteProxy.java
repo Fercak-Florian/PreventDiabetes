559543f537a8d93bc52cffecfceef8173e42d904
Fercak Florian <fercak.florian@hotmail.fr>
2023-04-18 16:00:21 +0200

    microservice report comunique de nouveau avec les microservices patient, note et view + generation des rapports de nouveau fonctionnelle

--- report/src/main/java/com/mediscreen/report/proxy/MicroserviceNoteProxy.java
+++ report/src/main/java/com/mediscreen/report/proxy/MicroserviceNoteProxy.java
@@ -2,14 +2,18 @@ package com.mediscreen.report.proxy;
 
 import com.mediscreen.report.bean.NoteBean;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @FeignClient(name = "note", url = "localhost:8082")
 public interface MicroserviceNoteProxy {
 
-    @GetMapping("/note/patientId/{id}")
-    List<NoteBean> getNotesByPatientId(@PathVariable int id);
+    @RequestMapping(value = "/note/patientId/{id}" ,method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    List<NoteBean> getNotesByPatientId(@PathVariable("id") int id);
 }
