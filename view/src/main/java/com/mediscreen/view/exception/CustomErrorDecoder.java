5bad202c3e321faddfdbce09aaa024f5d53b8dbe
Fercak Florian <fercak.florian@hotmail.fr>
2023-04-22 15:03:46 +0200

    suppression (filtre) du debut des url http dans CustomErrorDecoder, desactivation profile prod dans pom patient

--- view/src/main/java/com/mediscreen/view/exception/CustomErrorDecoder.java
+++ view/src/main/java/com/mediscreen/view/exception/CustomErrorDecoder.java
@@ -2,7 +2,9 @@ package com.mediscreen.view.exception;
 
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class CustomErrorDecoder implements ErrorDecoder {
 
     private final ErrorDecoder defaultErrorDecoder = new Default();
@@ -10,9 +12,18 @@ public class CustomErrorDecoder implements ErrorDecoder {
     @Override
     public Exception decode(String invoqueur, Response response) {
 
-        if (response.status() == 404 && response.request().toString().contains("http://localhost:8081/patient")) {
+        /*suppression du début de la requete http*/
+        String url = response.request().toString();
+        int index = url.indexOf(":");
+        url = url.substring(index + 1);
+        index = url.indexOf(":");
+        url = url.substring(index +1);
+        index = url.indexOf("/");
+        url = url.substring(index);
+
+        if (response.status() == 404 && url.startsWith("/patient")) {
             return new PatientNotFoundException("patient does not exist");
-        } else if (response.status() == 404 && response.request().toString().contains("http://localhost:8082/note")) {
+        } else if (response.status() == 404 && url.startsWith("/note")) {
             return new NoteNotFoundException("list is empty");
         }
         {
